81c1f690bd58eeba86bd2aef076a84531a67a3c2
Bryan Saw <bryansaw51@gmail.com>
2024-10-16 19:08:25 +0800

    firebase initial

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import ProfilePage from "./pages/ProfilePage";
 import BookingPage from "./pages/BookingPage";
+import { AuthProvider } from "./components/AuthProvider";
 
 
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ProfilePage />} />
-        <Route path="/login" element={<AuthPage />} />
-        <Route path="/bookings" element={<BookingPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="*" element={<AuthPage />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProfilePage />} />
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="/bookings" element={<BookingPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<AuthPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
